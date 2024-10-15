c8e95467cd031b950a715e8bab61a6e008af36eb
root <root@DESKTOP-NK8P9PO>
2024-10-15 16:38:06 +0100

    First commit

---
+++ contracts/SwapToken.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.27;
+import {IERC20} from "./IERC20.sol";
+
+contract SwapToken {
+    
+    IERC20 public baseToken;
+    IERC20 public celoToken;
+    uint256 public exchangeRate;
+
+    event SwapBase(
+        address indexed user,
+        uint256 baseAmount,
+        uint256 celoAmount
+    );
+    event SwapCelo(
+        address indexed user,
+        uint256 celoAmount,
+        uint256 baseAmount
+    );
+
+    event TokensDeposited(
+        address indexed depositor,
+        uint256 baseAmount,
+        uint256 celoAmount
+    );
+
+    
+    constructor(address _baseToken, address _celoToken, uint256 _exchangeRate) {
+        baseToken = IERC20(_baseToken);
+        celoToken = IERC20(_celoToken);
+        exchangeRate = _exchangeRate;
+    }
+
+    function swapBaseToCelo(uint256 _amount) public {
+        require(msg.sender != address(0), "Not allowed");
+        uint256 celoAmount = _amount * exchangeRate; 
+        require(
+            celoToken.balanceOf(address(this)) >= celoAmount,
+            "Not enough Celo Token"
+        ); 
+
+        require(
+            baseToken.transferFrom(msg.sender, address(this), _amount),
+            "Base transfer failed"
+        );
+
+        require(
+            celoToken.transfer(msg.sender, celoAmount),
+            "Celo transfer failed"
+        );
+        emit SwapBase(msg.sender, _amount, celoAmount);
+    }
+
+    function swapCeloToBase(uint256 _amount) public {
+        require(msg.sender != address(0), "Not allowed");
+
+        uint256 baseAmount = _amount / exchangeRate; 
+        require(
+            baseToken.balanceOf(address(this)) >= baseAmount,
+            "Not enough Base amount"
+        );
+
+        require(
+            celoToken.transferFrom(msg.sender, address(this), _amount),
+            "Celo transfer failed"
+        );
+
+        require(
+            baseToken.transfer(msg.sender, baseAmount),
+            "Base transfer failed"
+        );
+        emit SwapCelo(msg.sender, _amount, baseAmount);
+    }
+
+    
+    function depositTokens(uint256 baseAmount, uint256 celoAmount) public {
+        baseToken.transferFrom(msg.sender, address(this), baseAmount);
+        celoToken.transferFrom(msg.sender, address(this), celoAmount);
+
+        emit TokensDeposited(msg.sender, baseAmount, celoAmount);
+    }
+}
+
